d8b7c5e104a66c443de062ed269523910e692421
Andrii.Kuralekh <Andrii.Kuralekh@AKURALEKH-N>
2023-07-22 18:10:35 +0300

    Solution has been refactored

---
+++ Group3_Part2_HW2/Classes/Vehicle.cs
@@ -0,0 +1,12 @@
+namespace HW2_Cars_Controller
+{
+    public abstract class Vehicle    
+    {
+        public int year { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public int speed { get; set; }
+        public string engine { get; set; }
+
+    }
+}
---
+++ Group3_Part2_HW3/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        delegate void PrintString(string str);
+
+        static void Main(string[] args)
+        {
+            PrintString printString;
+            
+            var pr = new Print();
+            var initialString = "This is my string!";
+
+            printString  = new PrintString(pr.PrintStr);
+            printString += new PrintString(pr.PrintGreenStr);
+            printString += new PrintString(pr.PrintOrangeStr);
+
+            printString(initialString);
+
+            Console.WriteLine("The end");
+        }
+
+    }
+
+    public class Print
+    {
+        public void PrintStr(string str)
+        {
+            Console.WriteLine($"String: {str}");
+        }
+        public void PrintGreenStr(string str)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine($"String: {str}");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        public void PrintOrangeStr(string str)
+        {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"String: {str}");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
